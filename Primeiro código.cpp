595df0ee4f8a63304964bbad71d3a2d7f532030c
Luiz  Escobar <54407649+LuizEscobarC@users.noreply.github.com>
2020-10-06 18:45:41 -0400

    Create Primeiro código.cpp

---
+++ Primeiro código.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//adicionr a biblioteca iostream
+int main()
+//função principal
+{
+	//cout é o comando de saida de dados
+	std::cout << "Primeiro Codigo" << std::endl;
+	//endl quebra a linha
+	std::cout << "Eu sou o  Luiz" << std::endl;
+	system("PAUSE");
+	//pausa da execução
+	return 0;
+	//comando de termino base
+}
